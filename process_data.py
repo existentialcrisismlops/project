a8d611724f3822409bbff38f004ff019fcc375e4
fatima-aamir <aamirfatima350@gmail.com>
2023-12-07 20:11:25 +0500

    added mlflow

--- process_data.py
+++ process_data.py
@@ -2,31 +2,41 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-# Load the data from DVC-managed file
-data_file_path = 'dummy_sensor_data.csv'
-data = pd.read_csv(data_file_path)
+def preprocess_data(file_path='dummy_sensor_data.csv', test_size=0.2, random_state=42):
+    # Load the data from DVC-managed file
+    data = pd.read_csv(file_path)
 
-# Display a sample of the raw data
-print("Raw Data:")
-print(data.head())
+    # Display a sample of the raw data
+    print("Raw Data:")
+    print(data.head())
 
-# Extract features and target variable
-X = data[['Machine_ID', 'Sensor_ID', 'Reading']]
-y = data['Timestamp']
+    # Extract features and target variable
+    X = data[['Machine_ID', 'Sensor_ID', 'Reading']]
+    
+    # Convert timestamps to numerical values (e.g., seconds since a reference point)
+    data['Timestamp'] = pd.to_datetime(data['Timestamp'])
+    data['Timestamp'] = (data['Timestamp'] - data['Timestamp'].min()).dt.total_seconds()
+    y = data['Timestamp']
 
-# Convert categorical variables into numerical representation (if needed)
-X = pd.get_dummies(X)
+    # Convert categorical variables into numerical representation (if needed)
+    X = pd.get_dummies(X)
 
-# Normalize or scale features
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+    # Normalize or scale features
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
 
-# Split the data into training and validation sets
-X_train, X_val, y_train, y_val = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+    # Split the data into training and validation sets
+    X_train, X_val, y_train, y_val = train_test_split(X_scaled, y, test_size=test_size, random_state=random_state)
 
-# Display the processed data
-print("\nProcessed Data:")
-print("X_train shape:", X_train.shape)
-print("X_val shape:", X_val.shape)
-print("y_train shape:", y_train.shape)
-print("y_val shape:", y_val.shape)
+    # Display the processed data
+    print("\nProcessed Data:")
+    print("X_train shape:", X_train.shape)
+    print("X_val shape:", X_val.shape)
+    print("y_train shape:", y_train.shape)
+    print("y_val shape:", y_val.shape)
+
+    return X_train, X_val, y_train, y_val, scaler
+
+# If you want to run this script independently
+if __name__ == "__main__":
+    X_train, X_val, y_train, y_val, scaler = preprocess_data()
---
+++ train.py
@@ -0,0 +1,46 @@
+# train.py
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+import os
+import sys
+
+def train_model(X_train, y_train, X_val, y_val, n_estimators, max_depth, random_state):
+    with mlflow.start_run():
+
+        # Create and train the model
+        model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state)
+        model.fit(X_train, y_train)
+
+        # Make predictions on the validation set
+        y_val_pred = model.predict(X_val)
+
+        # Calculate the mean squared error
+        mse = mean_squared_error(y_val, y_val_pred)
+
+        # Log model parameters and metrics to MLflow
+        mlflow.log_params({
+            "n_estimators": n_estimators,
+            "max_depth": max_depth,
+            "random_state": random_state
+        })
+        mlflow.log_metric("mse", mse)
+
+        # Save the model
+        mlflow.sklearn.log_model(model, "model")
+
+        return model
+
+if __name__ == "__main__":
+    # Load preprocessed data
+    from process_data import preprocess_data
+    X_train, X_val, y_train, y_val, _ = preprocess_data()
+
+    # Get parameters from command line
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 10
+    random_state = int(sys.argv[3]) if len(sys.argv) > 3 else 42
+
+    # Train the model
+    trained_model = train_model(X_train, y_train, X_val, y_val, n_estimators, max_depth, random_state)
