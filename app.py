eea390ccd32d9a70c29a98313f5504fd4788f4b4
Alishba208 <alishba.nadeem208@gmail.com>
2024-01-03 02:07:24 -0800

    Changed app.py and docker file

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 import mlflow.sklearn
 import pandas as pd
 import mlflow
+import os
 from process_data import preprocess_data
 
-# Set the correct path to the mlruns directory on your machine
-mlflow.set_tracking_uri("file:///D:/Main/Semesters/7TH/mlops_work/project/project/mlruns")
-
 app = Flask(__name__)
+
 
 # Specify the MLflow experiment name and run ID
 mlflow_experiment_name = "wistful-wren-342"
 run_id = "5e4f59df99d742218fc5c9ae617bd397"
+artifact_uri = f"file:///proj/mlruns/0/{run_id}/artifacts/model"
 
-# Load the MLflow model
-model = mlflow.sklearn.load_model(f"runs:/{run_id}/model")
+loaded_model = mlflow.sklearn.load_model(artifact_uri)
 
 @app.route('/predict')
 def predict():
     # Get the input data (replace this with your actual input data)
     path = 'dummy_sensor_data.csv'
     x_val,_,_,_,_ = preprocess_data(path)
-    predictions = model.predict(x_val)
+    predictions = loaded_model.predict(x_val)
 
     # Log the predictions as a metric (replace this with your actual metrics)
     mlflow.log_metric("prediction_mean", predictions.mean())
@@ -29,4 +28,4 @@ def predict():
     return f"Predictions: {predictions.tolist()}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080,host='0.0.0.0')
